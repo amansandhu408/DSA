fb1fbe654fabea41382f498d7f59cc769e1b513b
amansandhu408 <amansandhu2241@gmail.com>
2025-09-27 15:32:49 +0530

    daily practice

---
+++ InterviewBit/Arrays/ARRAY_BUG.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rotateArray(vector<int> &A, int B);  // Declaration of the member function
+};
+
+
+vector<int> Solution::rotateArray(vector<int> &A, int B) {
+	vector<int> ret; 
+	for (int i = 0; i < A.size(); i++) {
+        B = B % (A.size());  // handle B > n
+		ret.push_back(A[(i + B)% (A.size())]);
+	}
+	return ret; 
+}
