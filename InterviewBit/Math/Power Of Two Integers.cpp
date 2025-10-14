8dca91d9261acafdcb8ef2b7ca95643d77fe681f
amansandhu408 <amansandhu2241@gmail.com>
2025-10-14 23:54:29 +0530

    daily practice

---
+++ InterviewBit/Math/Power Of Two Integers.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int isPower(int A,int B);
+};
+
+
+
+
+
+int Solution::isPower(int A) {
+    if (A == 1) return 1;  // 1 is always a power (1^P for any P)
+    
+    for (int base = 2; base * base <= A; base++) { // Start from base 2 and check till base^2 <= A
+        long long power = base;
+        while (power <= A) {
+            power *= base;
+            if (power == A) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
